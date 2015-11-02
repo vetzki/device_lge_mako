3e4339b5d1c3a5a8a24289b6298e53e6bd1d9492
mv <michvett.mv@gmail.com>
2015-11-02 13:01:26 +0100

    dex2oat: add filter speed
    from cm
    
    Change-Id: I2fbcbf2edbb961f5beab3e375e082fdef0f1b824

--- device.mk
+++ device.mk
@@ -285,6 +285,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
 PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.dex2oat-filter=speed \
         dalvik.vm.dex2oat-swap=false
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
