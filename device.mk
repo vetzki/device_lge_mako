3c81b59b58b8f6e6d4dbd3fac7867754f1f35ae5
mv <michvett.mv@gmail.com>
2016-02-10 16:36:52 +0100

    dex2oat: test filter
    Change-Id: I94ca057e953b86701a666e89cbd030a77a95f686

--- device.mk
+++ device.mk
@@ -285,8 +285,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.dex2oat-filter=speed \
-        dalvik.vm.dex2oat-swap=false
+	dalvik.vm.dex2oat-filter=everything \
+        dalvik.vm.dex2oat-swap=false \
+        dalvik.vm.image-dex2oat-filter=speed
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
